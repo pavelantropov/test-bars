07ca51381a8177ad1723016162fac24a77cfa76c
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-17 13:17:48 +0400

    Create a model for contracts

---
+++ src/Server/Models/Contract.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models;
+
+public class Contract
+{
+    public int Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public int Index { get; set; }
+    public DateTime UpdatedOn { get; set; }
+}
