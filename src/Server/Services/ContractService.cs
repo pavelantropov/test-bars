94a7531eedc822efede7149299c2e172a4e0816e
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-17 13:32:20 +0400

    Create a service class

---
+++ src/Server/Services/ContractService.cs
@@ -0,0 +1,29 @@
+﻿using Server.Models;
+using Server.Repositories;
+
+namespace Server.Services;
+
+public class ContractService : IItemService<Contract>
+{
+    private readonly IRepository<Contract> _repo;
+
+    public ContractService(IRepository<Contract> repo)
+    {
+        _repo = repo;
+    }
+
+    public Contract? Get(int id) =>
+        _repo.Get(id);
+
+    public IEnumerable<Contract> GetAll() =>
+        _repo.Items;
+
+    public void Add(Contract item) => 
+        _repo.Add(item);
+
+    public void Update(Contract updatedItem, int id) => 
+        _repo.Update(updatedItem, id);
+
+    public void Delete(int id) => 
+        _repo.Remove(id);
+}
---
+++ src/Server/Services/IItemService.cs
@@ -0,0 +1,13 @@
+﻿using Server.Models;
+
+namespace Server.Services;
+
+public interface IItemService<T>
+{
+    Contract? Get(int id);
+    IEnumerable<T> GetAll();
+
+    void Add(T item);
+    void Update(T updatedItem, int id);
+    void Delete(int id);
+}
