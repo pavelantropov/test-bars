f7389d9cfc87b35d48945faa5ac464494d695402
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-18 14:51:33 +0400

    Connect the client with the service

---
+++ src/ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using ClientApp.Models;
+using ContractsModel;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            const string serviceRoot = "http://localhost:59623/ContractsDataService.svc";
+            var context = new TaskBarsDbEntities(new Uri(serviceRoot));
+            ContractsListView.ItemsSource = context.Contracts.Select(c => new ContractTableModel()
+            {
+                Id = c.Id,
+                Index = c.Index,
+                CreatedOn = c.CreatedOn,
+                UpdatedOn = c.UpdatedOn,
+            });
+        }
+    }
+}
