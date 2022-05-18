b77cbd3e098dfd457f3fc136011ddaf44e46d421
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-18 14:46:55 +0400

    Remove the projects

--- src/Server/Models/Contract.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Models;
-
-public class Contract
-{
-    public int Id { get; set; }
-    public DateTime CreatedOn { get; set; }
-    public string Index { get; set; }
-    public DateTime UpdatedOn { get; set; }
-}
