47e2af09ffdc35bd11eccf1ea3719a88256a96a1
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-18 18:38:32 +0400

    Clean the service class a little

--- src/WcfService/ContractsDataService.svc.cs
+++ src/WcfService/ContractsDataService.svc.cs
@@ -3,16 +3,10 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
 using System.Data.Services;
 using System.Data.Services.Common;
 using System.Data.Services.Providers;
-using System.Linq;
-using System.Linq.Expressions;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Web;
 
 namespace WcfService
 {
@@ -22,10 +16,7 @@ namespace WcfService
         // This method is called only once to initialize service-wide policies.
         public static void InitializeService(DataServiceConfiguration config)
         {
-            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
-            // Examples:
-            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
-            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
             config.UseVerboseErrors = true;
         }
