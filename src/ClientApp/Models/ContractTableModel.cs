8ad7eccb5363efa4efed7fa4fce962d1a6dbdf50
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-18 14:51:12 +0400

    Create a model for populating the table

---
+++ src/ClientApp/Models/ContractTableModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClientApp.Models
+{
+    public class ContractTableModel
+    {
+        public int Id { get; set; }
+        public string Index { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+        public bool IsValid =>
+            (DateTime.UtcNow - UpdatedOn)?.Days < 30;
+    }
+}
